2d61b7bec8caac5a26b39bca89850684baa86ed4
= <mmonir.m.29@gmail.com>
2022-11-28 12:30:59 +0600

    :card_file_box: Alter CustomUser Table. Add user roles, update email fields to unique and not null.

--- authentication/models.py
+++ authentication/models.py
@@ -1,10 +1,25 @@
 
 
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class CustomUser(AbstractUser):
-
+    class UserRoles(models.TextChoices):
+        INSTRUCTOR = 'INS', _("Instructor")
+        LEARNER = 'LRN', _("Learner")
+    email = models.EmailField(
+        _("email address"),
+        unique=True,
+        null=False,
+    )
+    role = models.CharField(
+        _("User Roles"),
+        max_length=3,
+        choices=UserRoles.choices,
+        default=UserRoles.LEARNER
+    )
     @property
     def full_name(self):
         return '{} {}'.format(self.first_name, self.last_name)
